19688be4885b160ffa350dc1fae569d580f1b9d0
Mercy <aprilkasha42@gmail.com>
2020-06-16 11:36:09 +0300

    add a new date to quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
  
   quotes = [
-    new Quote(1, 'All our dreams can come true, if we have the courage to pursue them.',' Walt Disney', "W.D"),
-    new Quote(2, 'The secret of getting ahead is getting started.', 'Mark Twain',"M.T"),
-    new Quote(3, '“It’s hard to beat a person who never gives up.', 'Babe Ruth',"B.R"),
-    new Quote(4, 'Work like there is someone working twenty four hours a day to take it away from you.', 'Mark Cuban',"M.C"),
-    new Quote(5, 'Very often, a change of self is needed more than a change of scene.', 'L.H.Holts',"A.C.Benson"),
+    new Quote(1, 'All our dreams can come true, if we have the courage to pursue them.',' Walt Disney', "W.D",new Date(2020,3,14)),
+    new Quote(2, 'The secret of getting ahead is getting started.', 'Mark Twain',"M.T", new Date(2019,6,9)),
+    new Quote(3, '“It’s hard to beat a person who never gives up.', 'Babe Ruth',"B.R",new Date(2022,1,12)),
+    new Quote(4, 'Work like there is someone working twenty four hours a day to take it away from you.', 'Mark Cuban',"M.C",new Date(2019,0,18)), 
+    new Quote(5, 'Very often, a change of self is needed more than a change of scene.', 'L.H.Holts',"A.C.Benson",new Date(2019,2,14)),
 ];
 
 addNewQuote(quote){
